4a23c567b892b19ed1ab6a0ef3b68029f242230c
Jorge Sousa Pinto <jsp@di.uminho.pt>
2019-03-20 16:08:33 +0000

    added fibonacci fufunction and route

--- app.py
+++ app.py
@@ -3,10 +3,32 @@ import os
 from flask import Flask
 
 fibapp = Flask(__name__)
+
+class Config(object):
+    DEBUG = True
+    TESTING = False
+fibapp.config.from_object('app.Config')
+
+print('DEBUG:', fibapp.config['DEBUG'])
+print('TESTING:', fibapp.config['TESTING'])
+
+
 @fibapp.route('/') 
 def index():
     return "Hello World!"
-    
+
+
+@fibapp.route('/fib/<int:x>')
+def fib(x):
+    return str(calcfib(x))
+
+def calcfib(n):
+    if n == 0:
+        return 0
+    b, a = 0, 1             # b, a initialized as F(0), F(1)
+    for i in range(1,n) :
+        b, a = a, a+b       # b, a always store F(i-1), F(i) 
+    return a
 
 if __name__ == '__main__':
     fibapp.run()
